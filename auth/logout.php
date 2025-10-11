90b1ddcaa1890d2f4655ac59d3c32dea629e3b8e
voncheems <voncheems@gmail.com>
2025-10-11 21:51:59 +0800

    Create logout.php

---
+++ auth/logout.php
@@ -0,0 +1,20 @@
+<?php
+// logout.php
+// Place this file in the auth/ folder
+session_start();
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session cookie
+if (isset($_COOKIE[session_name()])) {
+    setcookie(session_name(), '', time() - 3600, '/');
+}
+
+// Destroy the session
+session_destroy();
+
+// Redirect to index page
+header('Location: /trimbook/index.php');
+exit;
+?>
