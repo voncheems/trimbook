a373e81c470be6cfa009f0c8813f7f77e2082c2b
voncheems <voncheems@gmail.com>
2025-10-21 19:47:52 +0800

    Update admin_managestatus.php - update repo

--- dashboards/admin_managestatus.php
+++ dashboards/admin_managestatus.php
@@ -1,5 +1,6 @@
 <?php
 session_start();
+require_once('../includes/dbconfig.php');
 
 // Get admin data from session
 $admin_name = $_SESSION['admin_name'] ?? 'Administrator';
@@ -12,13 +13,6 @@ $initials = strtoupper(substr($admin_name, 0, 2));
 $appointments = [];
 $db_error = null;
 
-try {
-    $conn = new mysqli("localhost", "root", "", "trimbookdb");
-    
-    if ($conn->connect_error) {
-        throw new Exception("Connection failed");
-    }
-    
     $query = "
         SELECT 
             a.appointment_id,
@@ -45,11 +39,6 @@ try {
         $appointments[] = $row;
     }
     
-    $conn->close();
-} catch (Exception $e) {
-    $db_error = "Error fetching appointments: " . $e->getMessage();
-}
-
 // Function to get status badge
 function getStatusBadge($status) {
     $badges = [
@@ -162,6 +151,13 @@ function formatDateTime($date, $time) {
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path>
           </svg>
           <span>All Appointments</span>
+        </a>
+
+        <a href="admin_addBarber.php" class="flex items-center space-x-3 px-4 py-3 rounded-xl text-gray-300 hover:bg-gray-800/50 hover:text-white transition">
+          <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z"></path>
+          </svg>
+          <span>Add Barber</span>
         </a>
 
         <a href="#" class="flex items-center space-x-3 px-4 py-3 rounded-xl text-gray-300 hover:bg-gray-800/50 hover:text-white transition">
